d5aefd38fafa87aa3f0c80178a9a533bd9bf5722
Qharny <cannycc1020@gmail.com>
2023-10-11 14:10:34 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   Angular-for-Beginners/.editorconfig
    	new file:   Angular-for-Beginners/.gitignore
    	new file:   Angular-for-Beginners/.hintrc
    	new file:   Angular-for-Beginners/.vscode/extensions.json
    	new file:   Angular-for-Beginners/.vscode/launch.json
    	new file:   Angular-for-Beginners/.vscode/tasks.json
    	new file:   Angular-for-Beginners/README.md
    	new file:   Angular-for-Beginners/angular.json
    	new file:   Angular-for-Beginners/package-lock.json
    	new file:   Angular-for-Beginners/package.json
    	new file:   Angular-for-Beginners/src/app/app-routing.module.ts
    	new file:   Angular-for-Beginners/src/app/app.component.css
    	new file:   Angular-for-Beginners/src/app/app.component.html
    	new file:   Angular-for-Beginners/src/app/app.component.spec.ts
    	new file:   Angular-for-Beginners/src/app/app.component.ts
    	new file:   Angular-for-Beginners/src/app/app.module.ts
    	new file:   Angular-for-Beginners/src/app/test/test.component.css
    	new file:   Angular-for-Beginners/src/app/test/test.component.html
    	new file:   Angular-for-Beginners/src/app/test/test.component.ts
    	new file:   Angular-for-Beginners/src/assets/.gitkeep
    	new file:   Angular-for-Beginners/src/favicon.ico
    	new file:   Angular-for-Beginners/src/index.html
    	new file:   Angular-for-Beginners/src/main.ts
    	new file:   Angular-for-Beginners/src/styles.css
    	new file:   Angular-for-Beginners/tsconfig.app.json
    	new file:   Angular-for-Beginners/tsconfig.json
    	new file:   Angular-for-Beginners/tsconfig.spec.json
    	new file:   binding/.editorconfig
    	new file:   binding/.gitignore
    	new file:   binding/.vscode/extensions.json
    	new file:   binding/.vscode/launch.json
    	new file:   binding/.vscode/tasks.json
    	new file:   binding/README.md
    	new file:   binding/angular.json
    	new file:   binding/package-lock.json
    	new file:   binding/package.json
    	new file:   binding/src/app/app-routing.module.ts
    	new file:   binding/src/app/app.component.css
    	new file:   binding/src/app/app.component.html
    	new file:   binding/src/app/app.component.spec.ts
    	new file:   binding/src/app/app.component.ts
    	new file:   binding/src/app/app.module.ts
    	new file:   binding/src/app/test/test.component.css
    	new file:   binding/src/app/test/test.component.html
    	new file:   binding/src/app/test/test.component.spec.ts
    	new file:   binding/src/app/test/test.component.ts
    	new file:   binding/src/assets/.gitkeep
    	new file:   binding/src/favicon.ico
    	new file:   binding/src/index.html
    	new file:   binding/src/main.ts
    	new file:   binding/src/styles.css
    	new file:   binding/tsconfig.app.json
    	new file:   binding/tsconfig.json
    	new file:   binding/tsconfig.spec.json

---
+++ Angular-for-Beginners/src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test', //'.' for class , '[]' for attributes
+  templateUrl: './test.component.html', /*
+  you can write the html code directly in here
+  multiple lines with back tick (`)
+  */
+  styleUrls: ['./test.component.css']
+  /* Starting with style: then [``]
+  you can now write your css inside
+  */
+})
+export class TestComponent {
+
+}
