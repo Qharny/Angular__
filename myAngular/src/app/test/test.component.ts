ceeee66170a783cd8d3f0323a356c4fedffb7340
Qharny <cannycc1020@gmail.com>
2023-10-16 10:34:57 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    binding/.editorconfig
    	deleted:    binding/.gitignore
    	deleted:    binding/.vscode/extensions.json
    	deleted:    binding/.vscode/launch.json
    	deleted:    binding/.vscode/tasks.json
    	deleted:    binding/README.md
    	deleted:    binding/angular.json
    	deleted:    binding/package-lock.json
    	deleted:    binding/package.json
    	deleted:    binding/src/app/app-routing.module.ts
    	deleted:    binding/src/app/app.component.css
    	deleted:    binding/src/app/app.component.html
    	deleted:    binding/src/app/app.component.spec.ts
    	deleted:    binding/src/app/app.component.ts
    	deleted:    binding/src/app/app.module.ts
    	deleted:    binding/src/app/test/test.component.html
    	deleted:    binding/src/assets/.gitkeep
    	deleted:    binding/src/favicon.ico
    	deleted:    binding/src/index.html
    	deleted:    binding/src/main.ts
    	deleted:    binding/src/styles.css
    	deleted:    binding/tsconfig.app.json
    	deleted:    binding/tsconfig.json
    	deleted:    binding/tsconfig.spec.json
    	modified:   myAngular/angular.json
    	modified:   myAngular/src/app/app.module.ts
    	renamed:    binding/src/app/test/test.component.css -> myAngular/src/app/test/test.component.css
    	new file:   myAngular/src/app/test/test.component.html
    	renamed:    binding/src/app/test/test.component.spec.ts -> myAngular/src/app/test/test.component.spec.ts
    	renamed:    binding/src/app/test/test.component.ts -> myAngular/src/app/test/test.component.ts

--- myAngular/src/app/test/test.component.ts
+++ myAngular/src/app/test/test.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `
-  <h1>My name is {{name}}</h1>
-  <h2>{{2+2}}</h2>
-  `,
+  templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
-  public name = 'Mr_Kabuteyy'
+
 }
